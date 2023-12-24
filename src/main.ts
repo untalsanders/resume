8422d291e6f5530d9da70841f44231a65211f28c
Sanders Gutiérrez <ing.sanders@gmail.com>
2023-12-24 00:57:37 -0300

    chore: add Poppins font
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser'
-import { AppComponent } from './app/app.component'
 import '@fontsource/poppins'
+import { AppComponent } from './app/app.component'
 
 bootstrapApplication(AppComponent).catch(e => console.error(e))
