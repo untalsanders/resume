c93a534d389f2726315d9e9d310d65bdbac5815e
Sanders Gutiérrez <ing.sanders@gmail.com>
2023-12-24 01:02:14 -0300

    fix: add Poppins font from Google Fonts
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser'
-import '@fontsource/poppins'
 import { AppComponent } from './app/app.component'
 
 bootstrapApplication(AppComponent).catch(e => console.error(e))
